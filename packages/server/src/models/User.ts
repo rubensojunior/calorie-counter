6c9265b7bdb513d7736a7abb998976f9946805df
rubensojunior <rubensojunior6@gmail.com>
2020-08-11 22:36:19 -0300

    configuring database

---
+++ packages/server/src/models/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  type: string
+
+  @Column('boolean')
+  isActive: boolean
+
+  @Column()
+  password: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
+
+export default User
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,16 @@
+import 'reflect-metadata'
+
 import express from 'express'
+
+import routes from './routes'
+
+import './database'
+
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Server stated on port 3333')
